ac86ee9ca92a8c36f91b387ca85d388996da744e
JosielJrr <josieljrr.dev@gmail.com>
2025-06-25 13:22:39 -0300

    Ajusta para configuração inicial

--- src/db.js
+++ src/db.js
@@ -1,25 +1,3 @@
 import { PrismaClient } from "@prisma/client";
-import fs from "fs";
-import path from "path";
 
-if (process.env.NODE_ENV === "production") {
-    const source = path.resolve("prisma/dev.db");
-    const destination = "/tmp/dev.db";
-
-    if (fs.existsSync(source) && !fs.existsSync(destination)) {
-        fs.copyFileSync(source, destination);
-    }
-}
-
-const isProd = process.env.NODE_ENV === "production";
-const databaseUrl = isProd ? "file:/tmp/dev.db" : process.env.DATABASE_URL;
-
-const db = new PrismaClient({
-    datasources: {
-        db: {
-            url: databaseUrl,
-        },
-    },
-});
-
-export default db;
+export const db = new PrismaClient();
